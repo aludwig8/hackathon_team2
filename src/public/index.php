6104ddacae55c943c73f86f11fbc514739086080
Kaltrina <kaltrinarushiti1@gmail.com>
2019-08-07 15:32:03 +0200

    Created the views for login and register page

--- src/public/index.php
+++ src/public/index.php
@@ -2,9 +2,14 @@
 
 require __DIR__ . '/../../src/config/app.php';
 
-$app->get('/home', function ($request, $response) {
-    return $this->view->render($response, 'home.twig');
-});
+$app->get('/login', function ($request, $response) {
+    return $this->view->render($response, 'login.twig');
+})->setName('login');
+
+$app->get('/register', function ($request, $response) {
+    return $this->view->render($response, 'register.twig');
+})->setName('register');
+
 
 $app->run();
 
