4a5d5dad90864d11646ef24108308d4af41b2e24
Kaltrina <kaltrinarushiti1@gmail.com>
2019-08-07 17:34:01 +0200

    The navbar was created and the pages 'home' and 'about' are finished.

--- src/public/index.php
+++ src/public/index.php
@@ -10,6 +10,18 @@ $app->get('/register', function ($request, $response) {
     return $this->view->render($response, 'register.twig');
 })->setName('register');
 
+$app->get('/index', function ($request, $response) {
+    return $this->view->render($response, 'index.twig');
+})->setName('home');
+
+$app->get('/about', function ($request, $response) {
+    return $this->view->render($response, 'about.twig');
+})->setName('about');
+
+$app->get('/products', function ($request, $response) {
+    return $this->view->render($response, 'products.twig');
+})->setName('products');
+
 
 $app->run();
 
