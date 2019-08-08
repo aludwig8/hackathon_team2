d5a03194a508ff71c89148eabb141783b3045b6d
irfan <irfanhasaniasl@gmail.com>
2019-08-08 08:29:51 +0200

    created separated file for routes

--- src/public/index.php
+++ src/public/index.php
@@ -2,26 +2,9 @@
 
 require __DIR__ . '/../../src/config/app.php';
 
-$app->get('/login', function ($request, $response) {
-    return $this->view->render($response, 'login.twig');
-})->setName('login');
-
-$app->get('/register', function ($request, $response) {
-    return $this->view->render($response, 'register.twig');
-})->setName('register');
-
-$app->get('/index', function ($request, $response) {
-    return $this->view->render($response, 'index.twig');
-})->setName('home');
-
-$app->get('/about', function ($request, $response) {
-    return $this->view->render($response, 'about.twig');
-})->setName('about');
-
-$app->get('/products', function ($request, $response) {
-    return $this->view->render($response, 'products.twig');
-})->setName('products');
-
+//register routes
+$routes = require __DIR__.'/../routes.php';
+$routes($app);
 
 $app->run();
 
---
+++ src/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+    $app->get('/login', function ($request, $response) {
+        return $this->view->render($response, 'login.twig');
+    })->setName('login');
+
+    $app->get('/register', function ($request, $response) {
+        return $this->view->render($response, 'register.twig');
+    })->setName('register');
+
+    $app->get('/index', function ($request, $response) {
+        return $this->view->render($response, 'index.twig');
+    })->setName('home');
+
+    $app->get('/about', function ($request, $response) {
+        return $this->view->render($response, 'about.twig');
+    })->setName('about');
+
+    $app->get('/products', function ($request, $response) {
+        return $this->view->render($response, 'products.twig');
+    })->setName('products');
+};
